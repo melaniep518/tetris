52b34cb5702eb335733a9afb47f90e0d86a45f79
Melanie Williams <melaniep518@gmail.com>
2016-11-14 17:36:43 -0500

    made lines more dynamic

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -4,43 +4,50 @@ import React from 'react';
 // ********************* IMPORT TETROMINOS *********************
 import {TetroContainer} from './containers/tetroContainer.js';
 
-// ********************* RENDER GAME GRID *********************
 const Grid = React.createClass({
 
-// THIS MUST BE MORE DYNAMIC WIDTH AND HEIGHT SHOULD BE DYNAMIC BASED ON SVG WIDTH AND HEIGHT;
-// AND INCREMENT SHOULD BE DYNAMIC BASED ON SCALE
-  
+// ********************* GRID COORDINATES *********************
+  coordinates: {
+    width: 200,
+    height: 400,
+    scale: 20
+  },
+
+// ********************* MAKES VERTICAL LINES ********************* 
   makeVerticalLines: function() {
     let verticalLines = [];
-    for(let width = 0; width <= 200; width += 20) {
+    for(let width = 0; width <= this.coordinates.width; width += this.coordinates.scale) {
       verticalLines.push(<line x1={width} x2={width} y1="0" y2="400" key={"vertLine_" + width} strokeWidth="0.5" stroke="black" />)
     }
     return verticalLines;
   },
 
+
+// ********************* MAKES HORIZONTAL LINES *********************
   makeHorizontalLines: function() {
     let horizontalLines = [];
-    for(let height = 0; height <= 400; height += 20) {
+    for(let height = 0; height <= this.coordinates.height; height += this.coordinates.scale) {
       horizontalLines.push(<line x1="0" x2="200" y1={height} y2={height} key={"horizLine_" + height} strokeWidth="0.5" stroke="black" />)
     }
     return horizontalLines;
   },
 
+
+// ********************* RENDER GAME GRID *********************
   render: function() {
     let style = {
       backgroundColor: "#eee"
     }
-    console.log(this.makeVerticalLines());
 
 // WIDTH & HEIGHT have been scaled to "gridSize" by multiplying the dimensions (10w x 20h)
     
     return (
       <div>
         <h1>Hello World!</h1>
-        <svg width="200px" height="400px" style={style}>
+        <svg width={this.coordinates.width} height={this.coordinates.height} style={style}>
             {this.makeVerticalLines()}
             {this.makeHorizontalLines()}
-            <TetroContainer gridSize="20" />   
+            <TetroContainer gridSize={this.coordinates.scale} />   
         </svg>
       </div>
     )
